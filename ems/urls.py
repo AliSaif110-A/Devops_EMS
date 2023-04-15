304cedf94db3e2021891645fbc2919493af21903
AliSaif110-A <125074821+AliSaif110-A@users.noreply.github.com>
2023-04-15 21:56:51 +0100

    Update urls.py

--- ems/urls.py
+++ ems/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     path('admin/', admin.site.urls,name="admin-site"),
     path('', include('employee_information.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
