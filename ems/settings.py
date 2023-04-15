adfc271e809601aaa33fdbf20c1986fa3039a6b3
AliSaif110-A <125074821+AliSaif110-A@users.noreply.github.com>
2023-04-15 21:15:20 +0100

    Update settings.py

--- ems/settings.py
+++ ems/settings.py
@@ -110,12 +110,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = ''
-
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS=[
 STATIC_DIR,
  ]
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
