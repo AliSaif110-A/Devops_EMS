e825bb7f04ce6a53650557c0e299d4dacc80c1af
AliSaif110-A <125074821+AliSaif110-A@users.noreply.github.com>
2023-04-15 22:19:54 +0100

    Update urls.py

--- ems/urls.py
+++ ems/urls.py
@@ -22,4 +22,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls,name="admin-site"),
     path('', include('employee_information.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
